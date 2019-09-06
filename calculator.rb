29b031674b23f7285f4b66f497171fe3fcf2134e
Vannida Lim <vannida731@gmail.com>
2019-09-06 04:56:52 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,18 @@
 # Add your variables here
+def first_number = Integer || Float
+end 
+
+def second_number != 0
+end
+
+def sum = first_number + second_number
+end
+
+def difference = first_number - second_number
+end
+
+def product = first_number * second_number
+end
+
+def quotient = first_number / second_number
+end 
