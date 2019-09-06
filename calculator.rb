87a409459001c7fbdace9c15563436bf2b281dc3
Vannida Lim <vannida731@gmail.com>
2019-09-06 04:56:01 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -7,3 +7,12 @@ end
 
 def sum = first_number + second_number
 end
+
+def difference = first_number - second_number
+end
+
+def product = first_number * second_number
+end
+
+def quotient = first_number / second_number
+end 
